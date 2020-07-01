e5114d82aaee24fc26863f116560885167329691
Tom Crossland <tom.crossland@bluetab.net>
2020-07-01 20:13:35 +0200

    chore: v1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## [1.0.0] 2020-07-01
+
+### Added
+
+- Breadth-first traversal with limit on number of levels
+
 ## [0.1.1] 2020-01-31
 
 ### Changed
--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Graph.MixProject do
   def project do
     [
       app: :graph,
-      version: "0.1.1",
+      version: "1.0.0",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
